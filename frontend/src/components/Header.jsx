5f858fea9850fc9cb104d5b13ab46b575d7078d2
Xiao <xmeng1@ualberta.ca>
2022-08-02 16:20:18 -0600

    Refactored base header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,18 +4,31 @@ import styles from '../styles/header.module.css';
 
 function Header() {
   return (
-    <header className={styles.header} role="navigation">
-      <Link to="/" className={styles.link}>
-        <h1 className={styles.h1}>Video Game Store</h1>
-      </Link>
-      <div className={styles.right}>
-        <Link to="store" className={`${styles.link} ${styles.underline}`}>
-          Store
+    <header>
+      <nav className={styles.main_navbar}>
+        <Link to="/" className={styles.left}>
+          <h1>Video Game Store</h1>
         </Link>
-        <Link to="cart" className={`${styles.link} ${styles.underline}`}>
-          Cart
-        </Link>
-      </div>
+        <div className={styles.right}>
+          <Link to="store" className={styles.red_on_hover}>
+            Store
+          </Link>
+          <Link to="cart" className={styles.red_on_hover}>
+            Cart
+          </Link>
+        </div>
+      </nav>
+      <nav className={styles.secondary_navbar}>
+        <button type="button" className={styles.red_on_hover}>
+          Games
+        </button>
+        <button type="button" className={styles.red_on_hover}>
+          Hardware
+        </button>
+        <button type="button" className={styles.red_on_hover}>
+          Merchandise
+        </button>
+      </nav>
     </header>
   );
 }
