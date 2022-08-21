743ce5dce98962f3db476aa701707a00ba105357
Xiao <xmeng1@ualberta.ca>
2022-08-20 22:42:00 -0600

    Fix store render bug

--- frontend/src/pages/Store.jsx
+++ frontend/src/pages/Store.jsx
@@ -1,22 +1,17 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 import styles from '../styles/store.module.css';
 
 import StoreCard from '../components/StoreCard';
-import {
-  selectProducts,
-  selectProductsIsIdle,
-} from '../redux/slices/productsSlice';
+import { selectProducts } from '../redux/slices/productsSlice';
 import fetchProducts from '../redux/thunks/fetchProducts';
 import useTestApiConnection from '../hooks/useTestApiConnection';
 
 function Store() {
   const products = useSelector(selectProducts);
-  const productsIsIdle = useSelector(selectProductsIsIdle);
   const dispatch = useDispatch();
   const [searchParams] = useSearchParams();
-  const productIds = useRef();
   const apiIsConnected = useTestApiConnection();
 
   useEffect(() => {
@@ -34,17 +29,13 @@ function Store() {
     }
   }, [searchParams, apiIsConnected]);
 
-  if (productsIsIdle) {
-    productIds.current = Object.keys(products);
-  }
-
   if (!apiIsConnected) {
     return <div className={styles.loading}>Waiting for API...</div>;
   }
 
   return (
     <div className={styles.container}>
-      {productIds.current.map((id) => (
+      {Object.keys(products).map((id) => (
         <StoreCard key={id} id={id} />
       ))}
     </div>
