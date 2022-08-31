1bf47a42a1a918810559a008476e550d0036a813
Xiao Meng <43506875+xiao-meng1@users.noreply.github.com>
2022-08-30 19:30:41 -0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,6 @@ const apiRouter = require('./routes/apiRouter');
 
 const app = express();
 
-//Mongoose connection
 mongoose.connect(`${process.env.MONGODB_URI}/store_data_test`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -23,36 +22,29 @@ db.on('error', () => {
   console.error('MongoDB connection error:');
 });
 
-// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// CORS
 app.use(
   cors({
     origin: [process.env.FRONTEND_ORIGIN, 'http://checkout.stripe.com'],
   })
 );
 
-// routers
 app.use('/products', productRouter);
 app.use('/checkout', checkoutRouter);
 app.use('/api', apiRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // send error
   res.status(err.status || 500);
   res.json({
     message: err.message,
